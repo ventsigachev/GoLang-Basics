c093fb7ac0bc1df54d5c8896444d84aaf65e2303
ventsigachev <vgachev@gmail.com>
2023-04-05 23:01:05 +0300

    Added Zoo_Shop

---
+++ 1.First_Steps/Zoo_Shop.go
@@ -0,0 +1,27 @@
+// Write a program to estimate necessities of food for dogs and cats.
+// One package of dog food costs 2.50 and cat food is 4.
+// Input:
+//		First line - packages of dog food
+//		Second line - packages of cat food 
+
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var dogPackages int
+	fmt.Scanln(&dogPackages)
+
+	var catPackages int
+	fmt.Scanln(&catPackages)
+
+	var dogCost = float64(dogPackages) * 2.50
+	var catCost = float64(catPackages) * 4
+
+	var allCost = dogCost + catCost
+
+	fmt.Println(allCost)
+
+}
